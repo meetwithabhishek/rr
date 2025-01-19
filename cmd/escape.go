3a08241a8e73130c2caf4a4df0f78a5827cae9ce
Abhishek_Kumar <abhishek.kumar@thalesgroup.com>
2025-01-19 14:49:42 +0530

    give alias to escape command

--- cmd/escape.go
+++ cmd/escape.go
@@ -10,8 +10,9 @@ import (
 
 // escapeCmd represents the escape command
 var escapeCmd = &cobra.Command{
-	Use:   "escape",
-	Short: "interpret the escape sequences",
+	Use:     "escape",
+	Aliases: []string{"e"},
+	Short:   "interpret the escape sequences",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		replacer := strings.NewReplacer(
 			`\\`, `\`,
@@ -36,14 +37,4 @@ var escapeCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(escapeCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// escapeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// escapeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
